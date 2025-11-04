6c0ed6800dff0053538c4215fcce1b77f0e1b1c8
ElishaParker <elishaparker@hotmail.com>
2025-11-04 17:58:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -79,8 +79,8 @@ async function runTracking() {
   const correctedY = offsetY * gainY;
 
   // Map to screen space (invert X)
-  let x = window.innerWidth  * (0.55 - correctedX);
-  let y = window.innerHeight * (0.6 + correctedY);
+  let x = window.innerWidth  * (0.45 - correctedX);
+  let y = window.innerHeight * (0.61 + correctedY);
 
 
   // --- 🔧 Global amplification multiplier ---

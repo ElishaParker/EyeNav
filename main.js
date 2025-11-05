9c07c7068f1d7340fd38b69b069be0f158d787b9
ElishaParker <elishaparker@hotmail.com>
2025-11-04 23:00:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ async function runTracking() {
   const correctedY = offsetY * gainY;
 
   // Map to screen space (invert X)
-  let x = window.innerWidth  * (0.481 - correctedX * 2.161);
+  let x = window.innerWidth  * (0.4805 - correctedX * 2.161);
   let y = window.innerHeight * (0.502 + correctedY * 3.5);
 
 
